c34480cf07ce94633f630539f20c5873bde5180c
Chinmay Behera <chinubehera7@gmail.com>
2025-05-18 20:32:45 +0530

    Remove unused sidebar state management from Dashboard component

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,11 +3,6 @@ import { Container, Navbar, Nav, Button, Offcanvas } from 'react-bootstrap';
 import RecordList from './RecordList';
 
 function Dashboard({ onLogout }) {
-  const [showSidebar, setShowSidebar] = useState(false);
-
-  const handleClose = () => setShowSidebar(false);
-  const handleShow = () => setShowSidebar(true);
-
   return (
     <Container fluid className="mt-3">
       {/* Main Content */}
